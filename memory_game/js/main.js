4c1ab10ff3d50e7d3d0682e59af103858a866270
issacg1 <yitzygrunwald@gmail.com>
2017-07-17 19:02:27 -0400

    added some JS logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,24 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"]
 
-console.log("user flipped " + cardThree);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
 console.log("user flipped " + cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped " + cardTwo);
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("you found a match")
+	}
+	else{
+		alert("sorry try again!")
+	}
+}
