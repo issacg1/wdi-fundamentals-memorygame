34407cfcb3afa25358fe69e596b9be86e1bfbe06
issacg1 <yitzygrunwald@gmail.com>
2017-07-18 16:45:13 -0400

    created some functions and cleaned up the if statments

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,22 @@
 var cards = ["queen", "queen", "king", "king"]
-
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId])
+	checkForMatch();
+}	
 
-cardsInPlay.push(cardOne);
-
-console.log("user flipped " + cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped " + cardTwo);
-
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("you found a match")
-	}
-	else{
-		alert("sorry try again!")
+var checkForMatch = function(){
+	if(cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			alert("you found a match")
+		}
+		else{
+			alert("sorry try again!")
+		}
 	}
 }
+
+flipCard(0);
+flipCard(2);
