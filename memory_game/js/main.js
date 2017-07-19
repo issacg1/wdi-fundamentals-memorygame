81e1653dfd0e14d020b04585d5ded4b29e4d623d
issacg1 <yitzygrunwald@gmail.com>
2017-07-19 18:09:17 -0400

    Added objects and 2 console.logs

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,38 @@
-var cards = ["queen", "queen", "king", "king"]
+var cards = [
+ {
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+ },
+ {
+ 	rank: "queen",
+ 	suit: "diamonds",
+ 	cardImage: "images/queen-of-diamonds.png",
+ },
+ {
+ 	rank: "king",
+ 	suit: "hearts",
+ 	cardImage: "images/king-of-hearts.png",
+ },
+ {
+ 	rank: "king",
+ 	suit: "diamonds",
+ 	cardImage: "images/king-of-diamonds.png",
+ },
+];
+
 var cardsInPlay = [];
 
+//To save the flipped cards into the holder Array.
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId])
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank)
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
 }	
 
+//To check that the holder Array contains two cards.
 var checkForMatch = function(){
 	if(cardsInPlay.length === 2){
 		if(cardsInPlay[0] === cardsInPlay[1]){
