7c51bcdb351075c9d2296fed764620f0c78316ee
issacg1 <yitzygrunwald@gmail.com>
2017-07-20 20:51:09 -0400

    All done

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,48 +1,62 @@
+console.log('up and running!');
 var cards = [
  {
 	rank: "queen",
 	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png",
+	cardImage: "images/queen-of-hearts.png"
  },
  {
  	rank: "queen",
  	suit: "diamonds",
- 	cardImage: "images/queen-of-diamonds.png",
+ 	cardImage: "images/queen-of-diamonds.png"
  },
  {
  	rank: "king",
  	suit: "hearts",
- 	cardImage: "images/king-of-hearts.png",
+ 	cardImage: "images/king-of-hearts.png"
  },
  {
  	rank: "king",
  	suit: "diamonds",
- 	cardImage: "images/king-of-diamonds.png",
+ 	cardImage: "images/king-of-diamonds.png"
  },
 ];
 
 var cardsInPlay = [];
 
 //To save the flipped cards into the holder Array.
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank)
+	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
-}	
+};	
 
-//To check that the holder Array contains two cards.
+/*This creates the card images and listens for when the user
+clicks on a card*/
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
+
+//To check that the holder Array contains two cards. Also, alerts results.
 var checkForMatch = function(){
-	if(cardsInPlay.length === 2){
-		if(cardsInPlay[0] === cardsInPlay[1]){
-			alert("you found a match")
+ if(cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("you found a match");
 		}
-		else{
-			alert("sorry try again!")
-		}
-	}
-}
+		else {
+			alert("sorry try again!");
+		};
+ 	};
+};
 
-flipCard(0);
-flipCard(2);
+createBoard();
